bec8e70963697281120421ddc70dff2b5b570798
RaviTejaVodela <raviteja.vodela@yahoo.com>
2023-03-05 21:08:15 +0530

    Services running on Local DB

---
+++ 05.BackendPractice/src/main/java/com/btp/backend/services/IAddressPersistence.java
@@ -0,0 +1,9 @@
+package com.btp.backend.services;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.btp.backend.entities.Address;
+
+public interface IAddressPersistence extends JpaRepository<Address,String>{
+
+}
